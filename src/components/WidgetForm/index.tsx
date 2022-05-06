e047fda458622e8696703858b58276b24cc3bad1
me-myauti <myauti.rafael@outlook.com>
2022-05-06 03:49:48 -0400

    Created folder Steps and added conditional rendering

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,7 +1,8 @@
-import { CloseButton } from "./CloseButton";
-import bugImageUrl from '../assets/bug.svg'
-import ideaImageUrl from '../assets/idea.svg'
-import thoughtImageUrl from '../assets/thought.svg'
+import { CloseButton } from "../CloseButton";
+import bugImageUrl from '../../assets/bug.svg'
+import ideaImageUrl from '../../assets/idea.svg'
+import thoughtImageUrl from '../../assets/thought.svg'
+import { useState } from "react";
 
 const feedbackTypes = {
     BUG: {
@@ -29,7 +30,12 @@ const feedbackTypes = {
     }
 };
 
+type FeedbackType = keyof typeof feedbackTypes;
+
 export function WidgetForm(){
+
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
     return(
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             <header>
@@ -37,12 +43,14 @@ export function WidgetForm(){
                 <CloseButton />
 
             </header>
-            <div className="flex py-8 gap-2 w-full">
+            {!feedbackType ? (
+                <div className="flex py-8 gap-2 w-full">
                 { Object.entries(feedbackTypes).map(([key, value])=>{
                     return(
                         <button
+                        key={key}
                             className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                            //onClick={}
+                            onClick={() => setFeedbackType(key as FeedbackType)}
                         >
                             <img src={value.image.source} alt={value.image.alt} />
                             <span>{value.title}</span>
@@ -50,6 +58,9 @@ export function WidgetForm(){
                     );
                 }) }
             </div>
+            ) : (
+                <p>Hello World</p>
+            )}
             <footer className="text-xs text-neutral-400">
                 Feito com ❤ pela <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
             </footer>
